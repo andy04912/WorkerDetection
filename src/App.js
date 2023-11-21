a8f2c5ce8e6614d93e78c6574f89d1c930888738
Brady <andy04912@yahoo.com.tw>
2023-11-21 13:49:05 +0800

    第一次

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Styles from "./App.module.scss";
+import { Outlet } from "react-router-dom";
+import Header from "./Compoents/Shared/Header";
+import Footer from "./Compoents/Shared/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={Styles.fixed_background}>
+			<div className={Styles.App}>
+				<Header></Header>
+				<div className={Styles.outletTop}>
+					<Outlet></Outlet>
+				</div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
